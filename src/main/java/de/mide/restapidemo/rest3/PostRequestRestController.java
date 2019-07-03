1b9ff35fcfb359959e43fdc8aba37b1949d120db
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-07-03 09:40:14 +0200

    Upload von Excel-Datei (Datei wird aber noch nicht ausgewertet).

---
+++ src/main/java/de/mide/restapidemo/rest3/PostRequestRestController.java
@@ -0,0 +1,92 @@
+package de.mide.restapidemo.rest3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+/**
+ * Diese RestController-Klasse zeigt wie Daten über einen HTTP-Post-Request empfangen werden können.
+ * Ein POST-Request anstelle eines GET-Requests sollte immer dann verwendet werden, wenn (Business-)Daten vom
+ * REST-Service gespeichert werden müssen.
+ * Im Gegensatz zu einem GET-Request kann ein POST-Request nicht einfach durch Eingabe einer URL in den
+ * Web-Browser abgesetzt werden; es wird dafür z.B. eine Browser-Erweiterung wie z.B.
+ * <a href="https://restlet.com/modules/client/" target="_blank">Restlet</a> für Chrome benötigt (siehe 
+ * <a href="https://github.com/MDecker-MobileComputing/Java_RestApi_SpringBoot/wiki/POST-Requests" target="_blank">diese Wiki-Seite</a>
+ * für die Verwendung dieser Browser-Erweiterung zum Aufruf der REST-Methode {@link #datenViaPostRequestEmpfangen(String)}).
+ * <br><br>
+ * 
+ * This project is licensed under the terms of the BSD 3-Clause License.
+ */
+@RestController
+@RequestMapping(value = "/rest3")
+public class PostRequestRestController {
+
+    /** Log-Objekt, auf das nur Methoden aus dieser Klasse schreiben dürfen. */
+    private static final Logger LOGGER = LoggerFactory.getLogger( PostRequestRestController.class );
+
+    
+    /**
+     * REST-Methode zum Empfang von Daten via POST-Request. Normalerweise würden diese Daten in einen Datenbank geschrieben,
+     * aber für dieses Beispiel schreiben wir die Daten nur auf den Logger.
+     * <br><br>
+     * 
+     * Bei lokaler Ausführung ist diese REST-Methode unter der folgenden URL verfügbar:
+     * <a target="_blank" href="http://localhost:8080/rest3/datenViaPostRequestEmpfangen">http://localhost:8080/rest3/datenViaPostRequestEmpfangen</a>
+     * <br><br>
+     * 
+     * Da POST-Requests nicht direkt mit dem Browser erzeugt werden können, findet sich (bei lokaler Ausführung) unter der folgenden
+     * URL eine HTML-Seite mit einem Formular, mit der Daten an diese REST-Methode geschickt werden können:
+     * <a target="_blank" href="http://localhost:8080/index.html">http://localhost:8080/index.html</a><br>
+     * 
+     * @param payloadString  Vom Client geschickte Nutzdaten, werden einfach auf Logger geschrieben, bei Übertragung
+     *                       von Web-Formular mit <a target="_blank" href="https://www.w3schools.com/tags/ref_urlencode.asp">URL-Kodierung</a> 
+     *                       (d.h. Sonderzeichen wie Leerzeichen und Umlaute durch "+" bzw. Prozent-Codes ersetzt).
+     * 
+     * @return  String mit Erfolgsmeldung für Client, z.B. "Daten (18 Zeichen) empfangen.".
+     */
+    @RequestMapping(value = "/datenViaPostRequestEmpfangen", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)    
+    public String datenViaPostRequestEmpfangen( @RequestBody String payloadString) {                    
+        
+        LOGGER.info("Daten via Post-Request empfangen: \"{}\"", payloadString);
+                
+        String antwortString = "Daten (" + payloadString.length() + " Zeichen) empfangen.";
+        
+        return antwortString; 
+    }
+    
+
+    /**
+     * Rest-Methode zum Empfang hochgeladener (Binär-)Datei.
+     * Siehe auch <a href="http://zetcode.com/springboot/uploadfile/" target="_blank">dieses Tutorial</a>.
+     * <br><br>
+     * 
+     * Bei lokaler Ausführung ist diese REST-Methode unter der folgenden URL verfügbar:
+     * <a target="_blank" href="http://localhost:8080/rest3/dateiHochladen">http://localhost:8080/rest3/dateiHochladen</a>
+     * 
+     * @param datei  Hochgeladene Datei (Parameter-Name muss mit Wert von Attribut {@code name} von {@code input}-Tag übereinstimmen).
+     * 
+     * @return  String mit Erfolgsmeldung für Client.
+     */
+    @RequestMapping(value = "/dateiHochladen", method = RequestMethod.POST, consumes = {"multipart/form-data"})            
+    public String dateiHochladen(@RequestParam MultipartFile datei) {
+        
+        // Attribute der Datei auslesen
+        String dateiName          = datei.getOriginalFilename();        
+        long   dateiGroesseBytes  = datei.getSize();        
+        String contentTypeString  = datei.getContentType();     
+        
+        String ergebnisStr = String.format("Datei mit Name \"%s\" empfangen (%d Bytes, Content Type \"%s\").", dateiName, dateiGroesseBytes, contentTypeString);
+        
+        LOGGER.info( ergebnisStr );
+                    
+        return ergebnisStr;
+    }
+    
+}
